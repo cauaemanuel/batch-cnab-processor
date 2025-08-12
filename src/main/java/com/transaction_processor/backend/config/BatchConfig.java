ca08381d0189a9361c510f12ab17722505b66e51
Araujo <cauaraujo@deloitte.com>
2025-08-12 09:57:57 -0300

    feat: criando o reader

--- src/main/java/com/transaction_processor/backend/config/BatchConfig.java
+++ src/main/java/com/transaction_processor/backend/config/BatchConfig.java
@@ -11,9 +11,13 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -45,6 +49,27 @@ public class BatchConfig {
                 .reader(itemReader)
                 .processor(itemProcessor)
                 .writer(itemWriter)
+                .build();
+    }
+
+    @Bean
+    FlatFileItemReader<TransacaoCNAB> reader(){
+        return new FlatFileItemReaderBuilder<TransacaoCNAB>()
+            .name("reader")
+                .resource(new FileSystemResource("file\\CNAB.txt"))
+                .fixedLength()
+                .columns(
+                        new Range(1,1), new Range(2,9),
+                        new Range(10,19), new Range(20,30),
+                        new Range(31,42), new Range(43,48),
+                        new Range(49,62), new Range(63,80)
+                )
+                .names(
+                    "tipo", "data","valor",
+                        "cpf","cartao","hora",
+                        "donoDaLoja","nomeDaLoja"
+                )
+                .targetType(TransacaoCNAB.class)
                 .build();
     }
 }
